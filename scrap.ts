bd886958ebcaedd14824750c30e4e816b546817d
diegomadariaga <diegonicolasmadariaga@gmail.com>
2021-11-09 00:20:27 -0300

    busqueda xbox en pcfactory

--- scrap.ts
+++ scrap.ts
@@ -1,13 +1,23 @@
 //const playwright = require('playwright');
-import  {chromium}  from 'playwright';
+import { chromium, Page } from "playwright";
 
 (async () => {
-    
-    const browser = await chromium.launch();
-    const page = await browser.newPage();
-    await page.goto('https://www.google.com');
-    await page.screenshot({path: 'google.png'});
+    const browser = await chromium.launch({ headless: false }); // para que se vea la ventana de chrome
+    const page: Page = await browser.newPage();
+    await page.goto("https://www.pcfactory.cl/xbox?categoria=664&papa=438");
+    //await page.screenshot({path: 'xbox.png'});
+    try {
+        let content = await page.textContent('[class="product-list"]');
+        console.log(content);
+        if(content?.includes("Xbox Series X")){
+            console.log("encontrado");
+        }else{
+            console.log("no encontrado");
+        }
+        
+    } catch (error) {
+        console.log("ha ocurrido un error: ", error);
+    }
 
     await browser.close();
-  
 })();
